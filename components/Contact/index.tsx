fa1a5162c1d723abfe243808ba7f5baaf1275719
Quang Linh <quanglink62@gmail.com>
2026-02-09 14:13:10 +0700

    9/2/25lan4

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,12 +1,57 @@
-"use client"; // nếu muốn sử dụng React hooks
+"use client";
 
+import { Textarea, TextInput, Button } from "@mantine/core";
+import styles from "./Contact.module.css";
 import React from "react";
 
 export default function ContactPage() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>Đây là trang liên hệ </p>
+    <div className={styles.wrapper}>
+      <h1 className={styles.pageTitle}>Liên hệ với chúng tôi</h1>
+
+      <div className={styles.card}>
+      
+
+        <div className={styles.grid}>
+          <TextInput
+            label="Họ và tên"
+            placeholder="Nhập họ và tên"
+            className={styles.input}
+                  withAsterisk
+          />
+
+          <TextInput
+            label="Email"
+            placeholder="Nhập email"
+                  withAsterisk
+            className={styles.input}
+          />
+
+          <TextInput
+            label="Chủ đề"
+            placeholder="Nhập chủ đề"
+                  withAsterisk
+            className={styles.input}
+          />
+
+          <TextInput
+            label="Số điện thoại"
+            placeholder="Nhập số điện thoại"
+                  withAsterisk
+            className={styles.input}
+          />
+        </div>
+
+        <Textarea
+              withAsterisk
+          label="Nội dung"
+          placeholder="Nhập nội dung tin nhắn"
+          minRows={4}
+          className={styles.textarea}
+        />
+
+        <Button className={styles.submitBtn}>Gửi</Button>
+      </div>
     </div>
   );
 }
