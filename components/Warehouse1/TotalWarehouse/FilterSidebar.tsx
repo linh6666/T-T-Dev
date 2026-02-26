f956b94380f3af95f0c7274a62b62ca2a3fc6ed8
Quang Linh <quanglink62@gmail.com>
2026-02-26 11:28:14 +0700

    26/2/25lan5

--- components/Warehouse1/TotalWarehouse/FilterSidebar.tsx
+++ components/Warehouse1/TotalWarehouse/FilterSidebar.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { MultiSelect } from "@mantine/core";
 import { WarehouseItem } from "./index";
+import styles from "./FilterSidebar.module.css";
 
 interface FilterSidebarProps {
+// ... existing props ...
   uniqueBuildingTypes: string[];
   selectedBuildingTypes: string[];
   setSelectedBuildingTypes: (val: string[]) => void;
@@ -52,6 +56,7 @@ interface FilterSidebarProps {
   setFilteredItems: (items: WarehouseItem[]) => void;
   setCurrentPage: (page: number) => void;
 }
+
 
 export default function FilterSidebar({
   uniqueBuildingTypes,
@@ -118,171 +123,271 @@ export default function FilterSidebar({
         Bộ lọc sản phẩm
       </h1>
 
-      {uniqueBuildingTypes.length > 0 && (
-        <MultiSelect
-          label="Loại công trình"
-          placeholder="Chọn loại công trình"
-          data={uniqueBuildingTypes}
-          value={selectedBuildingTypes}
-          onChange={setSelectedBuildingTypes}
-        />
-      )}
-
-      {uniqueDirections.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+      <div className={styles.filterListContainer}>
+        {uniqueBuildingTypes.length > 0 && (
           <MultiSelect
-            label="Hướng"
-            placeholder="Chọn hướng"
-            data={uniqueDirections}
-            value={selectedDirections}
-            onChange={setSelectedDirections}
+            label="Loại công trình"
+            placeholder="Chọn loại công trình"
+            data={uniqueBuildingTypes}
+            value={selectedBuildingTypes}
+            onChange={setSelectedBuildingTypes}
+            maxDropdownHeight={200}
+            styles={{
+              input: {
+                maxHeight: "100px",
+                overflowY: "auto",
+              },
+            }}
           />
-        </div>
-      )}
+        )}
 
-      {uniqueMainDoorDirections.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Hướng cửa chính"
-            placeholder="Chọn hướng cửa chính"
-            data={uniqueMainDoorDirections}
-            value={selectedMainDoorDirections}
-            onChange={setSelectedMainDoorDirections}
-          />
-        </div>
-      )}
+        {uniqueDirections.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Hướng"
+              placeholder="Chọn hướng"
+              data={uniqueDirections}
+              value={selectedDirections}
+              onChange={setSelectedDirections}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueBalconyDirections.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Hướng ban công"
-            placeholder="Chọn hướng ban công"
-            data={uniqueBalconyDirections}
-            value={selectedBalconyDirections}
-            onChange={setSelectedBalconyDirections}
-          />
-        </div>
-      )}
+        {uniqueMainDoorDirections.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Hướng cửa chính"
+              placeholder="Chọn hướng cửa chính"
+              data={uniqueMainDoorDirections}
+              value={selectedMainDoorDirections}
+              onChange={setSelectedMainDoorDirections}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFeature1.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Đặc điểm nổi bật 1"
-            placeholder="Chọn đặc điểm nổi bật 1"
-            data={uniqueFeature1}
-            value={selectedFeature1}
-            onChange={setSelectedFeature1}
-          />
-        </div>
-      )}
+        {uniqueBalconyDirections.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Hướng ban công"
+              placeholder="Chọn hướng ban công"
+              data={uniqueBalconyDirections}
+              value={selectedBalconyDirections}
+              onChange={setSelectedBalconyDirections}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFeature2.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Đặc điểm nổi bật 2"
-            placeholder="Chọn đặc điểm nổi bật 2"
-            data={uniqueFeature2}
-            value={selectedFeature2}
-            onChange={setSelectedFeature2}
-          />
-        </div>
-      )}
+        {uniqueFeature1.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Đặc điểm nổi bật 1"
+              placeholder="Chọn đặc điểm nổi bật 1"
+              data={uniqueFeature1}
+              value={selectedFeature1}
+              onChange={setSelectedFeature1}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFeature3.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Đặc điểm nổi bật 3"
-            placeholder="Chọn đặc điểm nổi bật 3"
-            data={uniqueFeature3}
-            value={selectedFeature3}
-            onChange={setSelectedFeature3}
-          />
-        </div>
-      )}
+        {uniqueFeature2.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Đặc điểm nổi bật 2"
+              placeholder="Chọn đặc điểm nổi bật 2"
+              data={uniqueFeature2}
+              value={selectedFeature2}
+              onChange={setSelectedFeature2}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFeature4.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Đặc điểm nổi bật 4"
-            placeholder="Chọn đặc điểm nổi bật 4"
-            data={uniqueFeature4}
-            value={selectedFeature4}
-            onChange={setSelectedFeature4}
-          />
-        </div>
-      )}
+        {uniqueFeature3.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Đặc điểm nổi bật 3"
+              placeholder="Chọn đặc điểm nổi bật 3"
+              data={uniqueFeature3}
+              value={selectedFeature3}
+              onChange={setSelectedFeature3}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueApartmentType.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Loại sản phẩm"
-            placeholder="Chọn loại sản phẩm"
-            data={uniqueApartmentType}
-            value={selectedApartmentType}
-            onChange={setSelectedApartmentType}
-          />
-        </div>
-      )}
+        {uniqueFeature4.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Đặc điểm nổi bật 4"
+              placeholder="Chọn đặc điểm nổi bật 4"
+              data={uniqueFeature4}
+              value={selectedFeature4}
+              onChange={setSelectedFeature4}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueApartmentModelCode.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Mã mẫu sản phẩm"
-            placeholder="Chọn mã mẫu"
-            data={uniqueApartmentModelCode}
-            value={selectedApartmentModelCode}
-            onChange={setSelectedApartmentModelCode}
-          />
-        </div>
-      )}
+        {uniqueApartmentType.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Loại sản phẩm"
+              placeholder="Chọn loại sản phẩm"
+              data={uniqueApartmentType}
+              value={selectedApartmentType}
+              onChange={setSelectedApartmentType}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueApartmentNum.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Số hiệu sản phẩm"
-            placeholder="Chọn số hiệu"
-            data={uniqueApartmentNum}
-            value={selectedApartmentNum}
-            onChange={setSelectedApartmentNum}
-          />
-        </div>
-      )}
+        {uniqueApartmentModelCode.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Mã mẫu sản phẩm"
+              placeholder="Chọn mã mẫu"
+              data={uniqueApartmentModelCode}
+              value={selectedApartmentModelCode}
+              onChange={setSelectedApartmentModelCode}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFloorName.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Tầng"
-            placeholder="Chọn tầng"
-            data={uniqueFloorName}
-            value={selectedFloorName}
-            onChange={setSelectedFloorName}
-          />
-        </div>
-      )}
+        {uniqueApartmentNum.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Số hiệu sản phẩm"
+              placeholder="Chọn số hiệu"
+              data={uniqueApartmentNum}
+              value={selectedApartmentNum}
+              onChange={setSelectedApartmentNum}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFloorGroupCode.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Nhóm tầng"
-            placeholder="Chọn nhóm tầng"
-            data={uniqueFloorGroupCode}
-            value={selectedFloorGroupCode}
-            onChange={setSelectedFloorGroupCode}
-          />
-        </div>
-      )}
+        {uniqueFloorName.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Tầng"
+              placeholder="Chọn tầng"
+              data={uniqueFloorName}
+              value={selectedFloorName}
+              onChange={setSelectedFloorName}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
 
-      {uniqueFloorApartmentGroupCode.length > 0 && (
-        <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
-          <MultiSelect
-            label="Nhóm sản phẩm của tầng"
-            placeholder="Chọn nhóm sản phẩm"
-            data={uniqueFloorApartmentGroupCode}
-            value={selectedFloorApartmentGroupCode}
-            onChange={setSelectedFloorApartmentGroupCode}
-          />
-        </div>
-      )}
+        {uniqueFloorGroupCode.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Nhóm tầng"
+              placeholder="Chọn nhóm tầng"
+              data={uniqueFloorGroupCode}
+              value={selectedFloorGroupCode}
+              onChange={setSelectedFloorGroupCode}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
+
+        {uniqueFloorApartmentGroupCode.length > 0 && (
+          <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
+            <MultiSelect
+              label="Nhóm sản phẩm của tầng"
+              placeholder="Chọn nhóm sản phẩm"
+              data={uniqueFloorApartmentGroupCode}
+              value={selectedFloorApartmentGroupCode}
+              onChange={setSelectedFloorApartmentGroupCode}
+              maxDropdownHeight={200}
+              styles={{
+                input: {
+                  maxHeight: "100px",
+                  overflowY: "auto",
+                },
+              }}
+            />
+          </div>
+        )}
+      </div>
 
       {sortedBedrooms.length > 0 && (
         <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", gap: "15px" }}>
